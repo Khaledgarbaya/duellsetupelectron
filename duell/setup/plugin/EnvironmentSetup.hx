15b9505ed2b581128b119c4a6eb19837401127b7
Khaledgarbaya <khaledgarbaya@gmail.com>
2015-10-07 15:21:41 +0200

    now asking the user if he wants to clone hxnidejs

--- duell/setup/plugin/EnvironmentSetup.hx
+++ duell/setup/plugin/EnvironmentSetup.hx
@@ -108,8 +108,29 @@ class EnvironmentSetup
     private function installHaxeNode(): Void
     {
         var targetPath: String = Path.join([DuellConfigHelper.getDuellConfigFolderLocation(), "lib"]);
+        var defaultHxNodeJSLibPath: String = Path.join([targetPath, "lib", "hxnodejs"]);
+        var hxnodejsPath: String = ""
+
+        var cloneAnswer = AskHelper.askYesOrNo("[Required]clone hxnodejs library from "+ hxNodeJSRepoUrl +"?");
+        var hxNodeJSLibPath = AskHelper.askString("hxnodejs library Location", defaultHxNodeJSLibPath);
+
+        if(hxnodejsPath == "")
+            hxnodejsPath = defaultHxNodeJSLibPath;
+
+        hxnodejsPath = resolvePath(hxnodejsPath);
+        if (cloneAnswer)
+        {
+            if(FileSystem.exists(hxnodejsPath))
+            {
+                FileSystem.deleteDirectory(hxnodejsPath);
+            }
+            CommandHelper.runCommand(targetPath, "git", ["clone", hxNodeJSRepoUrl], {errorMessage: "doawloading hxnodejs Lib"});
+            CommandHelper.runCommand(Path.join([targetPath, "lib", "hxnodejs"]), "haxelib",
+                                    ["dev", "hxnodejs", "."], {errorMessage: "setting hxnodejs as dev lib"});
+        }
+
         CommandHelper.runCommand(targetPath, "git", ["clone", hxNodeJSRepoUrl], {errorMessage: "doawloading hxnodejs Lib"});
-        CommandHelper.runCommand(Path.join([DuellConfigHelper.getDuellConfigFolderLocation(), "lib", "hxnodejs"]), "haxelib",
+        CommandHelper.runCommand(Path.join([targetPath, "lib", "hxnodejs"]), "haxelib",
                                 ["dev", "hxnodejs", "."], {errorMessage: "setting hxnodejs as dev lib"});
     }
 
